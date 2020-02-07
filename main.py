40d3007052b7ec549b0501df73282fe265b0bf4f
henry <1151071+HenrYxZ@users.noreply.github.com>
2020-02-06 22:48:39 -0600

    using shading function that goes from 0 to 1 instead of -1 to 1

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
+import time
 from PIL import Image
 import numpy as np
 # Local libraries
 import utils
 
 # Constants
+from constants import MAX_COLOR_VALUE
 NORMAL_MAP_FILENAME = "normal.jpg"
+OUTPUT_FILENAME = "img_out.png"
 L = utils.normalize(np.array([1, 1, 1]))
-MAX_COLOR_VALUE = 255
 
+def shade(n, l):
+    """
+    Shader calculation for a normal and a light vector.
+    Args:
+        n(numpy.array): Unit normal vector
+        l(numpy.array): Unit vector in the direction to the light
+    Returns:
+        numpy.uint8: The calculated color (grayscale 0-255)
+    """
+    diffuse_coef = (np.dot(n, l) + 1) / 2
+    color = np.uint8(np.maximum(0, diffuse_coef) * MAX_COLOR_VALUE)
+    return color
 
 def main():
+    start = time.time()
+    print("Opening Normal Map...")
     im = Image.open(NORMAL_MAP_FILENAME)
     # Create an array from the image
     im_array = np.asarray(im)
     # Create output image vector
     w, h, channels = im_array.shape
-    output = np.zeros((w, h))
+    output = np.zeros((w, h), dtype=np.uint8)
+    print("Shading image...")
     # Iterate over the array
-    counter = 0
     for i in range(w):
         for j in range(h):
             normal_map_pixel = im_array[j][i]
             n = utils.adjust(normal_map_pixel)
-            n_dot_L = np.dot(n, L)
-            output[j][i] = np.maximum(0, n_dot_L) * MAX_COLOR_VALUE
-            if counter % 1000 == 0:
-                print(
-                    "Iteration number {0}, i={1}, j={2}".format(counter, i, j)
-                )
-            counter += 1
+            output[j][i] = shade(n, L)
     # Turn output into image and show it
     im_output = Image.fromarray(output)
-    im_output.show()
+    im_output.save(OUTPUT_FILENAME)
+    print("Output image saved as {}".format(OUTPUT_FILENAME))
+    end = time.time()
+    elapsed_time = (end - start)
+    print("Elapsed time was: {}ms".format(elapsed_time * 1000))
+    print("or in human time: {}".format(utils.humanize_time(elapsed_time)))
+
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,10 +1,21 @@
 import numpy as np
+# Local Libraries
+from constants import MAX_COLOR_VALUE
+
 
 def normalize(v):
     norm = np.linalg.norm(v)
     if norm == 0:
        return v
     return v / norm
+
+def humanize_time(secs):
+    """
+    Extracted from http://testingreflections.com/node/6534
+    """
+    mins, secs = divmod(secs, 60)
+    hours, mins = divmod(mins, 60)
+    return '%02d:%02d:%02f' % (hours, mins, secs)
 
 def adjust(normal_map_pixel):
     """
@@ -19,7 +30,8 @@ def adjust(normal_map_pixel):
     """
     # Change B,G,R to R,G,B
     x, y, z = normal_map_pixel
-    x -= 128
-    y -= 128
-    normal_map_pixel = np.array((x, y, z)) / 255.0
+    half_range = (MAX_COLOR_VALUE + 1) / 2
+    x -= half_range
+    y -= half_range
+    normal_map_pixel = np.array((x, y, z)) / MAX_COLOR_VALUE
     return normalize(normal_map_pixel)
