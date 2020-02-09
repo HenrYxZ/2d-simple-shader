8a1a5a5ab5e6a3c73d5bdb6f798ab6ea79b57815
henry <1151071+HenrYxZ@users.noreply.github.com>
2020-02-09 00:33:17 -0600

    feat: blending between light and dark images

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ OUTPUT_FILENAME = "img_out.png"
 NORMAL_VECTORS_FILENAME = "normals.npy"
 SECOND_TO_MS = 1000
 NORMAL_DIMENSIONS = 3
+DARK_IMG_FILENAME = "dark_red.jpg"
+LIGHT_IMG_FILENAME = "light_red.jpg"
+RGB_CHANNELS = 3
 # The program will output an update every (this number) percent done
 PERCENTAGE_STEP = 10
 L = utils.normalize(np.array([1, 1, 1]))
@@ -56,9 +59,26 @@ def shade(n, l):
     color = np.uint8(np.maximum(0, diffuse_coef) * MAX_COLOR_VALUE)
     return color
 
+def shade_imgs(n, l, dark, light):
+    """
+    Shader calculation for a normal and a light vector and light and dark
+    pixel.
+    Args:
+        n(numpy.array): Unit normal vector
+        l(numpy.array): Unit vector in the direction to the light
+        dark(numpy.array): RGB pixel of dark image
+        light(numpy.array): RGB pixel of light image
+    Returns:
+        numpy.uint8: The calculated color (grayscale 0-255)
+    """
+    # This formula changes the value [-1 - 1] to [0 - 1]
+    diffuse_coef = (np.dot(n, l) + 1) / 2
+    t = np.maximum(0, diffuse_coef)
+    color = light * (1 - t) + dark * t
+    return color
+
 
 def use_normal_map():
-    start = time.time()
     print("Opening Normal Map...")
     normal_im = Image.open(NORMAL_MAP_FILENAME)
     # Create an array from the image
@@ -81,18 +101,42 @@ def use_normal_map():
         elapsed_time = utils.humanize_time(end_normals - start_normals)
         print("Time adjusting normals was: {}".format(elapsed_time))
     # Create output image vector
-    w, h, _ = normal_im_array.shape
+    w, h, _ = normals.shape
+    return normals, w, h
+
+
+def use_simple_shading(normals, w, h):
+    print("Shading using a simple shader...")
     output = np.zeros((h, w), dtype=np.uint8)
-    print("Shading image...")
-    start_shading = time.time()
-    # Iterate over the array
     for i in range(w):
         for j in range(h):
             n = normals[j][i]
             output[j][i] = shade(n, L)
-    end_shading = time.time()
-    elapsed_time = utils.humanize_time(end_shading - start_shading)
-    print("Time shading was: {}".format(elapsed_time))
+    return output
+
+
+def use_images(normals, w, h):
+    print("Opening dark image...")
+    dark_img = Image.open(DARK_IMG_FILENAME)
+    print("Opening light image...")
+    light_img = Image.open(LIGHT_IMG_FILENAME)
+    dark_array = np.asarray(dark_img)
+    light_array = np.asarray(light_img)
+    output = np.zeros((h, w, RGB_CHANNELS), dtype=np.uint8)
+    print("Shading between light and dark images...")
+    for i in range(w):
+        for j in range(h):
+            n = normals[j][i]
+            dark = dark_array[j][i]
+            light = light_array[j][i]
+            output[j][i] = shade_imgs(n, L, dark, light)
+    return output
+
+
+def main():
+    start = time.time()
+    normals, w, h = use_normal_map()
+    output = use_images(normals, w, h)
     # Turn output into image and show it
     im_output = Image.fromarray(output)
     im_output.save(OUTPUT_FILENAME)
@@ -101,10 +145,6 @@ def use_normal_map():
     elapsed_time = (end - start)
     print("Elapsed time was: {}ms".format(elapsed_time * SECOND_TO_MS))
     print("or in human time: {}".format(utils.humanize_time(elapsed_time)))
-
-
-def main():
-    use_normal_map()
 
 
 if __name__ == '__main__':
