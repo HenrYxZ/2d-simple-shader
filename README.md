a825dba35c79f4035aafb8f701bfa48f0b41c8b0
henry <1151071+HenrYxZ@users.noreply.github.com>
2020-02-10 22:39:33 -0600

    p01 final version

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions
+- Install Python 3
+- Install Numpy and Pillow python libraries (that can be done using pip, for ex. `$ pip install numpy`)
+- Run `$python -u main.py`
+- Follow the instructions
+
+Jesús Henríquez
--- main.py
+++ main.py
@@ -9,12 +9,18 @@ import utils
 from constants import MAX_COLOR_VALUE
 NORMAL_MAP_FILENAME = "normal.jpg"
 OUTPUT_FILENAME = "img_out.png"
-NORMAL_VECTORS_FILENAME = "normals.npy"
+NORMAL_VECTORS_FILENAME = "normals"
+NORMAL_VECTORS_FILE_EXT = ".npy"
 SECOND_TO_MS = 1000
 NORMAL_DIMENSIONS = 3
 DARK_IMG_FILENAME = "dark_red.jpg"
 LIGHT_IMG_FILENAME = "light_red.jpg"
 RGB_CHANNELS = 3
+DEFAULT_NORMALS_SIZE = 512
+CREATED_NORMALS_FILENAME = "created_normals.npy"
+CREATED_NORMALS_IMG_FILENAME = "created_normals.png"
+LIGHT_COLOR = np.array([232, 158, 39])
+DARK_COLOR = np.array([14, 5, 74])
 # The program will output an update every (this number) percent done
 PERCENTAGE_STEP = 10
 L = utils.normalize(np.array([1, 1, 1]))
@@ -33,7 +39,7 @@ def adjust_normal_map(rgb_normal_map):
     w, h, _ = rgb_normal_map.shape
     iterations = w * h
     step_size = np.ceil((iterations * PERCENTAGE_STEP) / 100).astype('int')
-    normals = np.zeros((w, h, NORMAL_DIMENSIONS))
+    normals = np.zeros((h, w, NORMAL_DIMENSIONS))
     counter = 0
     for i in range(w):
         for j in range(h):
@@ -55,51 +61,88 @@ def shade(n, l):
         numpy.uint8: The calculated color (grayscale 0-255)
     """
     # This formula changes the value [-1 - 1] to [0 - 1]
-    diffuse_coef = (np.dot(n, l) + 1) / 2
+    # diffuse_coef = (np.dot(n, l) + 1) / 2
+    diffuse_coef = np.dot(n, l)
     color = np.uint8(np.maximum(0, diffuse_coef) * MAX_COLOR_VALUE)
     return color
 
-def shade_imgs(n, l, dark, light):
+
+def shade_colors(n, l, dark, light):
     """
     Shader calculation for a normal and a light vector and light and dark
-    pixel.
+    colors.
     Args:
         n(numpy.array): Unit normal vector
         l(numpy.array): Unit vector in the direction to the light
-        dark(numpy.array): RGB pixel of dark image
-        light(numpy.array): RGB pixel of light image
+        dark(numpy.array): RGB dark color
+        light(numpy.array): RGB light color
     Returns:
-        numpy.uint8: The calculated color (grayscale 0-255)
+        numpy.uint8: The calculated color (RGB)
     """
     # This formula changes the value [-1 - 1] to [0 - 1]
-    diffuse_coef = (np.dot(n, l) + 1) / 2
+    # diffuse_coef = (np.dot(n, l) + 1) / 2
+    diffuse_coef = np.dot(n, l)
     t = np.maximum(0, diffuse_coef)
     color = light * (1 - t) + dark * t
     return color
 
 
-def use_normal_map():
-    print("Opening Normal Map...")
-    normal_im = Image.open(NORMAL_MAP_FILENAME)
+def create_normal_map():
+    print("Creating normal map...")
+    # iterate 512x512 array
+    normals = np.zeros((
+        DEFAULT_NORMALS_SIZE, DEFAULT_NORMALS_SIZE, NORMAL_DIMENSIONS
+    ))
+    x0 = DEFAULT_NORMALS_SIZE / 2 - 1
+    y0 = DEFAULT_NORMALS_SIZE / 2 - 1
+    iterations = DEFAULT_NORMALS_SIZE ** 2
+    step_size = np.ceil((iterations * PERCENTAGE_STEP) / 100).astype('int')
+    counter = 0
+    for i in range(DEFAULT_NORMALS_SIZE):
+        for j in range(DEFAULT_NORMALS_SIZE):
+            # create [j][i] normal
+            x = i - x0
+            y = j - y0
+            z = np.sqrt(DEFAULT_NORMALS_SIZE**2 - np.absolute(x * y))
+            normals[j][i] = np.array([x, y, z]) * MAX_COLOR_VALUE
+            counter += 1
+            if counter % step_size == 0:
+                percent_done = int((counter / float(iterations)) * 100)
+                print("{}% of normal map created".format(percent_done))
+    normals_array = normals.astype(np.uint8)
+    normals_img = Image.fromarray(normals_array)
+    normals_img.save("created_normals.png")
+    return normals_img
+
+
+def use_normal_map(normal_img, normal_opt):
     # Create an array from the image
-    normal_im_array = np.asarray(normal_im)
-    if os.path.exists("normals.npy"):
+    normal_im_array = np.asarray(normal_img)
+    normals_filename = (
+            NORMAL_VECTORS_FILENAME + normal_opt + NORMAL_VECTORS_FILE_EXT
+    )
+    if os.path.exists(normals_filename):
         # Load normals from file
-        print("Loading normal vectors from file {}".format(NORMAL_VECTORS_FILENAME))
-        normals = np.load(NORMAL_VECTORS_FILENAME)
-    else:
-        # Create the normals vector map
-        start_normals = time.time()
-        normals = adjust_normal_map(normal_im_array)
-        np.save(NORMAL_VECTORS_FILENAME, normals)
         print(
-            "Normal vectors stored inside {} file".format(
-                NORMAL_VECTORS_FILENAME
+            "Loading normal vectors from file {}".format(
+                normals_filename
             )
         )
-        end_normals = time.time()
-        elapsed_time = utils.humanize_time(end_normals - start_normals)
-        print("Time adjusting normals was: {}".format(elapsed_time))
+        normals = np.load(normals_filename)
+        w, h, _ = normals.shape
+        return normals, w, h
+    # Create the normals vector map
+    start_normals = time.time()
+    normals = adjust_normal_map(normal_im_array)
+    np.save(normals_filename, normals)
+    print(
+        "Normal vectors stored inside {} file".format(
+            normals_filename
+        )
+    )
+    end_normals = time.time()
+    elapsed_time = utils.humanize_time(end_normals - start_normals)
+    print("Time adjusting normals was: {}".format(elapsed_time))
     # Create output image vector
     w, h, _ = normals.shape
     return normals, w, h
@@ -129,14 +172,53 @@ def use_images(normals, w, h):
             n = normals[j][i]
             dark = dark_array[j][i]
             light = light_array[j][i]
-            output[j][i] = shade_imgs(n, L, dark, light)
+            output[j][i] = shade_colors(n, L, dark, light)
+    return output
+
+
+def use_colors(normals, w, h):
+    output = np.zeros((w, h, RGB_CHANNELS), dtype=np.uint8)
+    print("Shading between light and dark colors...")
+    for i in range(w):
+        for j in range(h):
+            n = normals[j][i]
+            output[j][i] = shade_colors(n, L, DARK_COLOR, LIGHT_COLOR)
     return output
 
 
 def main():
     start = time.time()
-    normals, w, h = use_normal_map()
-    output = use_images(normals, w, h)
+    normals_opt = input(
+        "Enter [1] to use normal map from image or [2] for using a function\n"
+    )
+    # normals_opt = '2'
+    if normals_opt == '2':
+        if os.path.exists(CREATED_NORMALS_IMG_FILENAME):
+            # Load normals from file
+            print(
+                "Opening previously created normal map {}".format(
+                    CREATED_NORMALS_FILENAME
+                )
+            )
+            normal_img = Image.open(CREATED_NORMALS_IMG_FILENAME)
+        else:
+            normal_img = create_normal_map()
+    else:
+        print("Opening Normal Map...")
+        normal_img = Image.open(NORMAL_MAP_FILENAME)
+    # Create a normal vector field from an image map
+    normals, w, h = use_normal_map(normal_img, normals_opt)
+    # Start shading using the normals
+    shading_opt = input(
+        "Enter [1] to use dark and light images or [2] to use colors or [3] to use grayscale\n"
+    )
+    # shading_opt = '1'
+    if shading_opt == '2':
+        output = use_colors(normals, w, h)
+    elif shading_opt == '3':
+        output = use_simple_shading(normals, w, h)
+    else:
+        output = use_images(normals, w, h)
     # Turn output into image and show it
     im_output = Image.fromarray(output)
     im_output.save(OUTPUT_FILENAME)
